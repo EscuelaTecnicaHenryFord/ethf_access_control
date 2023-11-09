e4b39aa77f1a73f626dd42f7c42f1362965b3274
Tomás Cichero <martoxdlol@gmail.com>
2023-11-09 11:19:10 -0300

    mejorar búsqueda

--- app/lib/search.dart
+++ app/lib/search.dart
@@ -49,21 +49,43 @@ class SearchResults extends StatelessWidget {
   final String filter;
 
   List<RemotePerson> filterList(List<RemotePerson> people) {
+    String trans(String s) {
+      return removeDiacritics(s).toLowerCase().trim().replaceAll(',', '');
+    }
+
+    final filter = trans(this.filter);
+
     if (filter.isEmpty) {
       return people;
     }
 
-    String trans(String s) {
-      return removeDiacritics(s).toLowerCase().trim();
-    }
+    final words = filter.split(' ').where((element) => element.isNotEmpty).toList();
 
-    return people.where((p) {
-      if (trans(p.name).contains(trans(filter))) return true;
-      if (trans(p.displayCuil).contains(trans(filter))) return true;
-      if (p.invitedBy != null && trans(p.invitedBy!).contains(trans(filter))) return true;
-      if (p.username != null && trans(p.username!).contains(filter.toLowerCase())) return true;
-      return false;
+    final matchLevels = people.map((p) {
+      int level = 10;
+
+      if (trans(p.name).contains(filter)) level++;
+      if (trans(p.displayCuil).contains(filter)) level++;
+      if (p.invitedBy != null && trans(p.invitedBy!).contains(filter)) level++;
+      if (p.username != null && trans(p.username!).contains(filter)) level++;
+
+      if (level == 10) {
+        level = 0;
+
+        for (final word in words) {
+          if (trans(p.name).contains(word)) level++;
+          if (trans(p.displayCuil).contains(word)) level++;
+          if (p.invitedBy != null && trans(p.invitedBy!).contains(word)) level++;
+          if (p.username != null && trans(p.username!).contains(word)) level++;
+        }
+      }
+
+      return (p, level);
     }).toList();
+
+    matchLevels.sort((a, b) => b.$2.compareTo(a.$2));
+
+    return matchLevels.where((element) => element.$2 > 0).map((e) => e.$1).toList();
   }
 
   @override
@@ -86,7 +108,8 @@ class SearchResults extends StatelessWidget {
           itemBuilder: (context, index) {
             return ListTile(
               title: Text(list[index].name),
-              subtitle: Text(list[index].id),
+              subtitle: Text(
+                  "${list[index].id == '' ? list[index].dni.toString() : list[index].id} - ${list[index].typeName}"),
               onTap: () => showPersonPage(context, list[index]),
             );
           },
